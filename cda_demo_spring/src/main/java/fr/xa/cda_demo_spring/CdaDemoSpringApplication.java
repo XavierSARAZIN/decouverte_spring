5794c49306f71cb8b0327fda5acc7345939b786f
Xa <xav54350@gmail.com>
2025-04-17 11:13:04 +0200

    Implementation variable d'environnement

--- cda_demo_spring/src/main/java/fr/xa/cda_demo_spring/CdaDemoSpringApplication.java
+++ cda_demo_spring/src/main/java/fr/xa/cda_demo_spring/CdaDemoSpringApplication.java
@@ -25,5 +25,6 @@ public class CdaDemoSpringApplication {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    
 }
 
--- cda_demo_spring/src/main/java/fr/xa/cda_demo_spring/security/SecuriteUtils.java
+++ cda_demo_spring/src/main/java/fr/xa/cda_demo_spring/security/SecuriteUtils.java
@@ -2,12 +2,16 @@ package fr.xa.cda_demo_spring.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
 public class SecuriteUtils {
+    @Value("${jwt.secret}")
+    String jwtSecret;
+
     public String getRole(AppUserDetails userDetails) {
         return userDetails.getAuthorities().stream()
                 .map(r -> r.getAuthority())
@@ -16,16 +20,17 @@ public class SecuriteUtils {
     }
 
     public String generateToken(AppUserDetails userDetails) {
+        
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .addClaims(Map.of("role", getRole(userDetails)))
-                .signWith(SignatureAlgorithm.HS256, "azerty")
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
                 .compact();
     }
 
     public String getSubjectFromJwt(String jwt) {
         return Jwts.parser()
-                .setSigningKey("azerty")
+                .setSigningKey(jwtSecret)
                 .parseClaimsJws(jwt)
                 .getBody()
                 .getSubject();
